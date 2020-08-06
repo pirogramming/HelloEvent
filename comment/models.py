30bcccf25463ed955f6e5b9c34200cd33ca3f7d7
InjiChoi <chldlswl1212@gmail.com>
2020-08-06 17:51:23 +0900

    comment model create

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from login import User, Creator
 
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE, related_name='comments')
+    comment_text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    approved_comment = models.BooleanField(default=True)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+        
+    def __str__(self):
+        return self.comment_text
+
+    
