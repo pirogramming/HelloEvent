e2e043517985ee118234886258096b3780d6d521
kimkiyun <kiyoon9390@gmail.com>
2020-08-06 17:48:49 +0900

    1차 머지전 긴장의 순간

--- event/models.py
+++ event/models.py
@@ -3,4 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
-    title = models.CharField(max_length = 500)
+    location = models.ForeignKey(Event_Location, on_delete=models.CASCADE)
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=200, blank=True)
+    desc = models.TextField(blank=True)
+    e
+
+class EventImage(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="event/%Y/%m/%d")
+
