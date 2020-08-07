356fbe929b6e39d45889c949ca67a65cda3f11d9
InjiChoi <chldlswl1212@gmail.com>
2020-08-07 22:15:56 +0900

    테스트 login model 오타 수정

--- login/models.py
+++ login/models.py
@@ -8,15 +8,11 @@ class Member(User):
 class Creator(models.Model):
     member = models.OneToOneField(Member, on_delete=models.CASCADE)
     creator_name = models.CharField(max_length=100, verbose_name='크리에이터명')
-    creator_photo = models.ImageField(upload_to="creator_photo/%Y/%m/%d/") #4주년 축하해 잘 보냈니? upload_to가 확실하지 않다. \
-    # 내가 하고 싶은건 업로드 날마다 날짜명이 기록된 다른 폴더에 들어가게 하는거야 찾아봐
+    creator_photo = models.ImageField(upload_to="creator_photo/%Y/%m/%d/")
     desc = models.TextField(blank=True)
-
-    # like_users = models.ManyToManyField(Member, on_delete = models.CASCADE)
 
 
 class Like(models.Model):
     from_user = models.ForeignKey(Member, on_delete=models.CASCADE)
     to_user = models.ForeignKey(Creator, on_delete=models.CASCADE)
-
 
