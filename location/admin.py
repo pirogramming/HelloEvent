6ecf59759f974f91815bc9227fe07553063e9c9d
Genie <327jhl@naver.com>
2020-08-07 21:00:41 +0900

    location 모델 완성

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Prefer_Location, Event_Location
 # Register your models here.
+admin.site.register(Prefer_Location)
+admin.site.register(Event_Location)
