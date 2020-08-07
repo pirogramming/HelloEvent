8d606247c90f43cfcfe77475a1c8c3e39c8541b9
InjiChoi <chldlswl1212@gmail.com>
2020-08-07 21:00:46 +0900

    comment model 완성하고나서 중간 커밋

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Comment
+from .forms import *
 
 # Create your views here.
+def comment_create():
+    pass
+
+def comment_update():
+    pass
+
+def comment_delete():
+    pass
