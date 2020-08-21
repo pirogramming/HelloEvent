3d8ada9f64fbc69ad0e1f6b22296d428f14e6685
Genie <327jhl@naver.com>
2020-08-21 17:26:24 +0900

    Ajax 문제점(다른 지역에서 장르버튼을 눌렀을 때 전체 이벤트가 뜨는 문제) 해결 및 시작 시간으로 정렬 및 이미지 없는 이벤트에 기본 이미지 삽입

--- location/views.py
+++ location/views.py
@@ -6,6 +6,9 @@ from event.models import Event,EventImage
 from location.models import Event_Location
 from django.views.decorators.csrf import csrf_exempt
 import json
+from datetime import datetime
+from django.db.models import Q
+
 def show_map(request):
     prefer_city = Member.objects.get(id=request.user.pk).city
     prefer_gu = Member.objects.get(id=request.user.pk).gu
@@ -16,12 +19,16 @@ def show_map(request):
     return render(request,'location/map_list.html',ctx)
 
 def search_map(request):
+    today = datetime.today()
+    print(today)
     if request.method == "POST":
         selected_city = request.POST.get('selected_city') 
         selected_gu = request.POST.get('selected_gu') 
         if selected_city == '시' or selected_gu == '구':
             return redirect('login:main')
-        events = Event.objects.all()
+        event_all = Event.objects.all()
+        events = event_all.filter(end_date_time__gte=today)
+        print(events)
         ctx = {
             'selected_city' : selected_city,
             'selected_gu' : selected_gu,
@@ -32,8 +39,18 @@ def search_map(request):
 
 @csrf_exempt
 def ajax_search_genre(request):
+    today = datetime.today()
     genre = request.POST.get('genre',None)
-    events = Event.objects.filter(genre=genre)
+    selected_city = request.POST.get('selected_city',None)
+    selected_gu = request.POST.get('selected_gu',None)
+
+    locs = Event_Location.objects.all().filter(Q(city__exact=selected_city) & Q(gu__exact=selected_gu))
+    events = Event.objects.none()
+    for loc in locs:
+        q = loc.event_set.all().filter(Q(genre__iexact = genre) & Q(end_date_time__gte=today))
+        events = events | q
+    events = events.order_by('start_date_time')
+    print(events)
     locations = Event_Location.objects.all()
     creators = Creator.objects.all()
     eventimages = EventImage.objects.all()
@@ -49,8 +66,7 @@ def ajax_search_genre(request):
     response_data['location'] = location
     response_data['creator'] = creator
 
-    print('eventimage 모델 : ',eventimage)
-    print('event 모델 : ',event)
-    print('evreator 모델 : ',creator)
+    # print('eventimage 모델 : ',eventimage)
+    # print('event 모델 : ',event)
+    # print('evreator 모델 : ',creator)
     return JsonResponse(response_data)
-        
